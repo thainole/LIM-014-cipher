831ae603d1f61cbec12ede296e8a26792ce0cd6c
thainole <thainole@gmail.com>
2021-01-20 11:30:10 -0500

    Proyecto Final

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,65 @@
-const cipher = {
-  // ...
+
+const cipher = 
+{
+  encode: function(number,text)
+  {//array de las letras permitidas
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let finalString = "";
+     
+    /* hago mi for para definir los parámetros */
+    for (let i=0; i<text.length; i++)
+    {
+      let word = text[i]; 
+      let validWord = letters.includes(word.toUpperCase())
+      /*si la palabra es válida, la convertimos a mayúsculas para encontrar la posicion ascii */
+      if (validWord == true){
+        let upperLetter = word.toUpperCase();
+        /*para la posición ascii, aplicamos charCodeAt y luego la fórmula sgte */ 
+        let asciiNumber = ((upperLetter.charCodeAt(0)-65+number)%26)+65;
+        /* pasamos la posición ascii (número) a la letra correspondiente */
+        let finalWord = String.fromCharCode(asciiNumber);
+        finalString += finalWord;
+      } 
+      else
+      {
+       /* Si se agrega otro tipo de caracter que no sea letra, no se convierte, solo se añade */
+        finalString += word; 
+      }      
+    }
+    return finalString;   
+  }, 
+
+  decode: function(number,text)
+  {
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let finalString = "";
+
+    for (let i=0; i<text.length; i++)
+    {
+      let word = text[i];
+      console.log(word);
+      let validWord = letters.includes(word.toUpperCase())
+
+      if (validWord==true)
+      {
+        let upperLetter = word.toUpperCase();
+        console.log(upperLetter);
+        let asciiNumber = (upperLetter.charCodeAt(0)+65-number)%26+65;
+        console.log(asciiNumber);
+        let finalWord = String.fromCharCode(asciiNumber);
+        console.log(finalWord);
+        finalString += finalWord;
+        console.log(finalString);
+      }
+      else 
+      {
+        finalString += word;
+      }
+    }
+    return finalString;
+  }
+  
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+//Declaro las variables que utilizaré :D 
+const encodeButton = document.getElementById("encode-btn");
+const decodeButton = document.getElementById("decode-btn");
+const clearButton = document.getElementById("clear")
+let encodeInput = document.getElementById("text-to-encode");
+let decodeInput = document.getElementById("text-to-decode");
+let offsetNumber = document.getElementById("number");
+let encodedText = "";
+let decodedText = "";
+
+// Al darle click a mi flecha de codificar, ocurre esta función:
+encodeButton.addEventListener("click", cifrar);
+function cifrar(){
+    let text = encodeInput.value;
+    let number = parseInt(offsetNumber.value); 
+    encodedText = cipher.encode(number,text);
+    decodeInput.value = encodedText;
+}
+    
+// Al darle click a mi flecha de decodificar, ocurre esta función:
+decodeButton.addEventListener("click", descifrar);
+function descifrar(){
+    let text = decodeInput.value;
+    let number = parseInt(offsetNumber.value);
+    decodedText = cipher.decode(number,text);
+    encodeInput.value = decodedText;
+}
+
+// Al darle click al botón limpiar, sucede esto: 
+clearButton.addEventListener("click",() => {
+    encodeInput.value="";
+    decodeInput.value="";
+    offsetNumber.value="";
+})
